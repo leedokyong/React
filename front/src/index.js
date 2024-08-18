12bb0558a80ff20c9fdcc6305fb42f47937366b7
ehrud3513 <ehrud3513@naver.com>
2024-08-18 20:30:31 +0900

    First commit "리액트 연습"

--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App'; //App  뒤에 .js가 생략되있다
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
